44587285b08857e67e616d7dfb5cfa42ce2934e6
160419982021 <huallpafernandez@gmail.com>
2022-01-25 20:34:17 +0000

    Structure of the code_game
    Snake game

---
+++ Game_snake_Tkinter.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False) #Can be change the X-Axis and Y-Axis, respectivament (x,y) using TRUE and FALSE
+window.mainloop()
