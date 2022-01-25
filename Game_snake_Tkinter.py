bd0046fc5e720e293732a21832fa3c138e30c2f0
160419982021 <huallpafernandez@gmail.com>
2022-01-25 21:27:12 +0000

    Counting in the score food
    Does not recognise the ending of the black background yet.

--- Game_snake_Tkinter.py
+++ Game_snake_Tkinter.py
@@ -51,11 +51,25 @@ def next_turn(snake, food):
 
     snake.squares.insert(0, square)
 
-    del snake.coordinates[-1]
+    if x == food.coordinates[0] and y == food.coordinates[1]:
 
-    canvas.delete(snake.squares[-1])
+        global score
 
-    del snake.squares[-1]
+        score += 1
+
+        label.config(text="Score:{}".format(score))
+
+        canvas.delete("food")
+
+        food = Food()
+
+    else:
+
+        del snake.coordinates[-1]
+
+        canvas.delete(snake.squares[-1])
+
+        del snake.squares[-1]
 
     window.after(SPEED, next_turn, snake, food)
 
