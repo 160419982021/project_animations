709d5506e74c6629a9be579b519973addfe315f9
160419982021 <huallpafernandez@gmail.com>
2022-01-25 20:44:27 +0000

    Score in the Tkinter

--- Game_snake_Tkinter.py
+++ Game_snake_Tkinter.py
@@ -3,6 +3,12 @@ import random
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00" #Color snake = GREEN from [HEX]. Others= RGB, CMYK, HSV, HSL
+FOOD_COLOR = "#FF0000" #Color food = RED from [HEX].
+BACKGROUND_COLOR = "#000000" #Color background = Black from [HEX].
 
 class Snake:
     pass
@@ -25,4 +31,13 @@ def game_over():
 window = Tk()
 window.title("Snake game")
 window.resizable(False, False) #Can be change the X-Axis and Y-Axis, respectivament (x,y) using TRUE and FALSE
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+
+
 window.mainloop()
