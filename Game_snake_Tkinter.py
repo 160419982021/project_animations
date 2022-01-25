c445e695601537cd0545a498acf516416f80a7f9
160419982021 <huallpafernandez@gmail.com>
2022-01-25 20:59:41 +0000

    Define the snake

--- Game_snake_Tkinter.py
+++ Game_snake_Tkinter.py
@@ -11,7 +11,17 @@ FOOD_COLOR = "#FF0000" #Color food = RED from [HEX].
 BACKGROUND_COLOR = "#000000" #Color background = Black from [HEX].
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 class Food:
     def __init__(self):
