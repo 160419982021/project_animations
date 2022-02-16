6d9757492b88661e1fad0b8bd908f0d5227013c5
160419982021 <huallpafernandez@gmail.com>
2022-02-16 10:00:40 +0000

    Feet of Watt

--- Game_World and characters/Game_World_and_Characters.py
+++ Game_World and characters/Game_World_and_Characters.py
@@ -11,15 +11,23 @@ client.put_pixels(leds)
 client.put_pixels(leds)
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Floor~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~Character 1 Watt~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 led = 0
 while led<60:                 
     for led in range(52):   
-        leds = [(0,0,0)]*360 #Whole simulator black R,G,B (0,0,0)
+        leds = [(0,0,255)]*360 #Whole simulator black R,G,B (0,0,0)
+        #~~~~~~~~~~~~~~~~~~~~~~~Feet(Black)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+        leds[351-led+2] = (0,0,0)
+        leds[351-led+3] = (0,0,0)
+        leds[351-led+4] = (0,0,0)
 
+        leds[351-led+6] = (0,0,0)
+        leds[351-led+7] = (0,0,0)
+        leds[351-led+8] = (0,0,0)
+        
 
         
         client.put_pixels(leds)
-        time.sleep(.01)
+        time.sleep(.5)
     break
