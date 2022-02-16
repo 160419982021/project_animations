7e2633321a19aacdb523e764849554bec964f1b8
160419982021 <huallpafernandez@gmail.com>
2022-02-16 09:44:07 +0000

    Floor of Game World 1

--- Game_World and characters/Game_World_and_Characters.py
+++ Game_World and characters/Game_World_and_Characters.py
@@ -1,1 +1,25 @@
 #Position  of the led in the game World and Characters
+
+import opc
+import time
+import random
+
+leds =[(0,0,0)]*360
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Floor~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+led = 0
+while led<60:                 
+    for led in range(52):   
+        leds = [(0,0,0)]*360 #Whole simulator black R,G,B (0,0,0)
+
+
+        
+        client.put_pixels(leds)
+        time.sleep(.01)
+    break
