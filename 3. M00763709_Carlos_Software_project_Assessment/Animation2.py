d85bbb0ed1099e22a2e273d05863f484b2a1785c
160419982021 <huallpafernandez@gmail.com>
2022-02-19 17:33:07 +0000

    Animation update

--- 3. M00763709_Carlos_Software_project_Assessment/Animation2.py
+++ 3. M00763709_Carlos_Software_project_Assessment/Animation2.py
@@ -1,24 +1,32 @@
 #Animation2
 
-
 import opc
 import time
 import random
 
-leds =[(64,64,64)]*360
+leds =[(250,250,0)]*360
 
 client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
+##1. Presentation of Thwomp
+led = 0
+while led<60:
+    for rows in range(2):
+        leds[led + rows*60] = (255,0,0) # Primeras 3 lineas de izquierda a derecha
+        
+    for rows in range(4,6):
+        leds[59-led + rows*60] = (255,0,0) # Ultimas 3 lineas de derecha a izquierda
+    client.put_pixels(leds)
+    time.sleep(.05)
+    led = led + 1
 
 
+##2. Show The Wall
 #~~~~~~~~~~~~~~~~~~~~~~~Character 2 Thwomp (Wall)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #Only position of Thwomp (Wall)
-##led = 60
-##while led<60:                 
-##    for led in range(-54):
-##    
+
 led = 0
 leds = [(0,0,255)]*360 #Whole simulator White R,G,B (x,x,x)
         #~~~~~~~~~~~~~~~~~~~~~~~Body(Grey -(160,160,160))~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -26,13 +34,13 @@ leds = [(0,0,255)]*360 #Whole simulator White R,G,B (x,x,x)
 time.sleep(1)
 #First line
 
-leds[0+led] = (160,160,160)      #ffsdfsdfdsf
-leds[0+led+1] = (160,160,160)
-leds[0+led+3] = (160,160,160)
+leds[0-led] = (160,160,160)      #ffsdfsdfdsf
+leds[0-led+1] = (160,160,160)
+leds[0-led+3] = (160,160,160)
 
-leds[0+led+5] = (160,160,160)
-leds[0+led+7] = (160,160,160)
-leds[0+led+8] = (160,160,160)
+leds[0-led+5] = (160,160,160)
+leds[0-led+7] = (160,160,160)
+leds[0-led+8] = (160,160,160)
 
 #Second Line
 leds[60+led] = (160,160,160)
@@ -66,7 +74,7 @@ leds[180+led+7] = (160,160,160)
 leds[180+led+8] = (160,160,160)
 
 #Five Line
-leds[240+led+1] = (160,160,160)
+leds[240-led+1] = (160,160,160)
 leds[240+led+2] = (255,0,0)
 leds[240+led+3] = (255,0,0)
 leds[240+led+4] = (255,0,0)
@@ -82,19 +90,15 @@ leds[300+led+6] = (160,160,160)
 leds[300+led+8] = (160,160,160)
 
 client.put_pixels(leds)
-time.sleep(0.5)
-#break
+time.sleep(5)
 
-###2.(rigth to left)
-##led = 0
-##while led<60:
-##    leds[59-led] = (0,255,0)
-##    leds[119-led] = (0,255,0)
-##    leds[179-led] = (0,255,0)
-##    leds[239-led] = (0,255,0)
-##    leds[299-led] = (0,255,0)
-##    leds[359-led] = (0,255,0)
-##
-##    time.sleep(.05)
-##    client.put_pixels(leds)
-##    led = led + 1
+
+###3. Close the curtain 
+led = 0
+while led<30: # si cambio a led<60 al cruzarse se cambian los colores
+    for rows in range(6):
+        leds[led + rows*60] = (255,0,0)
+        leds[59-led + rows*60] = (255,0,0)
+    client.put_pixels(leds)
+    time.sleep(.02)
+    led = led + 1
--- 3. M00763709_Carlos_Software_project_Assessment/Animation3.py
+++ 3. M00763709_Carlos_Software_project_Assessment/Animation3.py
@@ -1,1 +1,22 @@
 #Animation3
+# Animation for the ghost list go 1 by 1
+import opc
+import time
+import random
+
+leds =[(250,250,0)]*360
+
+client = opc.Client('localhost:7890')
+client.put_pixels(leds)
+client.put_pixels(leds)
+
+p = 0
+print (enumerate(leds))
+while True:
+    Ghost = [25,34,91,105,206,214,268,327,311,333]
+    leds[Ghost[p]] = (0,0,0)
+    client.put_pixels(leds)
+    time.sleep(1)
+    p = p + 1
+    if p == 10:
+        break
