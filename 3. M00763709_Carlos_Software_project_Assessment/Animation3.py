2abceb9bedca48aa50928668d75081dd5f4d544a
160419982021 <huallpafernandez@gmail.com>
2022-03-02 19:59:44 +0000

    Update

--- 3. M00763709_Carlos_Software_project_Assessment/Animation3.py
+++ 3. M00763709_Carlos_Software_project_Assessment/Animation3.py
@@ -1,158 +1,179 @@
-#Animation3
-# Animation for the ghost list go 1 by 1
-import opc
-import time
-import random
+#Animation3 - Created by Carlos Huallpa - M00763709 - 2nd Year Robotics and Mechatronics
 
-leds =[(250,250,0)]*360
-
-client = opc.Client('localhost:7890')
-client.put_pixels(leds)
-client.put_pixels(leds)
+import opc                                              #Import Open Pixel Control
+import time                                             #Import time access and conversions 
+import random                                           #Import and generate pseudo-random numbers 
 
 
+def A3():                                               #DEF - keyword for defining a function
 
-#7.(Block from bottom to up) serpiente que recorre todo el panel de leds al reves
-led = 0
-while True:                 #hacer el movimiento para siempre 
-    for led in range(0, 360, 60):   # en que rango queiro que el movimiento ocurra 
-        leds = [(255,255,255)]*360      #todo de un color
-        leds[330-led+1] = (0,0,255)
-        leds[331-led+1] = (0,0,255)
-        leds[332-led+1] = (0,0,255)
-        leds[333-led+1] = (0,0,255)
-        leds[334-led+1] = (0,0,255)
-        leds[335-led+1] = (0,0,255)
-        leds[336-led+1] = (0,0,255)
-        leds[337-led+1] = (0,0,255)
-        leds[338-led+1] = (0,0,255)
-        leds[339-led+1] = (0,0,255)
-        leds[340-led+1] = (0,0,255)
-        leds[341-led+1] = (0,0,255)
-        leds[342-led+1] = (0,0,255)
-        leds[343-led+1] = (0,0,255)
-        leds[344-led+1] = (0,0,255)
-        leds[345-led+1] = (0,0,255)
-        leds[346-led+1] = (0,0,255)
-        leds[347-led+1] = (0,0,255)
-        leds[348-led+1] = (0,0,255)
-        leds[349-led+1] = (0,0,255)
-        leds[350-led+1] = (0,0,255)
-        leds[351-led+1] = (0,0,255)
-        leds[352-led+1] = (0,0,255)
-        leds[353-led+1] = (0,0,255)
-        leds[354-led+1] = (0,0,255)
-        leds[355-led+1] = (0,0,255)
-        leds[355-led+2] = (0,0,255)
-        leds[355-led+3] = (0,0,255)
-        leds[355-led+4] = (0,0,255)
-
+    ##A. Set up - PINK
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         
-        if led == 255:                  #si alcanza el final que vuelva al inicio
-            led = 0
-        client.put_pixels(leds)
-        time.sleep(.1)
-    break
+    leds =[(255,51,255)]*360                            #Simulator color [R,G,B] * numbers of leds working - PINK
 
+    client = opc.Client('localhost:7890')               #Use the Simulator_Fadecandy
+    client.put_pixels(leds)                             #Use the Simulator_Fadecandy - LED's
+    client.put_pixels(leds)                             #Use the Simulator_Fadecandy - LED's
+    time.sleep(1)                                       #Number in seconds - 1s
+    
+    
+    ##B. MOVEMENT OF THE BLOCK (UPWARDS) - BLUE
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#8. (Block from up to bottom) va en columnas descendientes - no en filas        
-led = 0
-while True:                 #hacer el movimiento para siempre 
-    for rows in range(6):   # en que rango queiro que el movimiento ocurra 
-        leds = [(255,255,255)]*360      #todo de un color
+    led = 0                                             #Assignment of the LED equal to 0
+    while True:                                         #While true loop = execute a block of code repeatedly until given boolean condition evaluated to False. If we write while True then the loop will run forever.
+        for led in range(0, 360, 60):                   #For loop in range(X) movement of code bellow from 360 to 0 in steps of 60 positions of the LED
+            leds = [(255,255,255)]*360                  #Whole simulator Blue R,G,B (X,X,X) - WHITE
+            leds[330-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[331-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[332-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[333-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[334-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[335-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[336-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[337-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[338-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[339-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[340-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[341-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[342-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[343-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[344-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[345-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[346-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[347-led+1] = (0,255,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[348-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[349-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[350-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[351-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[352-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[353-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[354-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[355-led+1] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[355-led+2] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[355-led+3] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
+            leds[355-led+4] = (0,0,255)                 #Position of the LED - assignment - Simulator color [R,G,B] - BLUE
 
-        leds[led + rows*60] = (0,0,255)
-        leds[led+1 + rows*60] = (0,0,255)
-        leds[led+2 + rows*60] = (0,0,255)
-        leds[led+3 + rows*60] = (0,0,255)
-        leds[led+4 + rows*60] = (0,0,255)
-        leds[led+5 + rows*60] = (0,0,255)
-        leds[led+6 + rows*60] = (0,0,255)
-        leds[led+7 + rows*60] = (0,0,255)
-        leds[led+8 + rows*60] = (0,0,255)
-        leds[led+9 + rows*60] = (0,0,255)
-        leds[led+10 + rows*60] = (0,0,255)
-        leds[led+11 + rows*60] = (0,0,255)
-        leds[led+12 + rows*60] = (0,0,255)
-        leds[led+13 + rows*60] = (0,0,255)
-        leds[led+14 + rows*60] = (0,0,255)
-        leds[led+15 + rows*60] = (0,0,255)
-        leds[led+16 + rows*60] = (0,0,255)
-        leds[led+17 + rows*60] = (0,0,255)
-        leds[led+18 + rows*60] = (0,0,255)
-        leds[led+19 + rows*60] = (0,0,255)
-        leds[led+20 + rows*60] = (0,0,255)
-        leds[led+21 + rows*60] = (0,0,255)
-        leds[led+22 + rows*60] = (0,0,255)
-        leds[led+23 + rows*60] = (0,0,255)
-        leds[led+24 + rows*60] = (0,0,255)
-        leds[led+25 + rows*60] = (0,0,255)
-        leds[led+26 + rows*60] = (0,0,255)
-        leds[led+27 + rows*60] = (0,0,255)
-        leds[led+28 + rows*60] = (0,0,255)
-        leds[led+29 + rows*60] = (0,0,255)
-        leds[led+30 + rows*60] = (0,0,255)
-
-        client.put_pixels(leds)
-        time.sleep(.1)
-    break
-
-#~~~~~~~~~~~~~~~~~~~~~~~Character 3 The Grump (Mushroom)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#Moving from right to left
-led = 0
-while led<60:                 
-    for led in range(54-7):
             
-        leds = [(10,10,10)]*360 #Whole simulator White R,G,B (x,x,x)
+            if led == 255:                              #When the LED reach/equal to 255 will do the code bellow
+                led = 0                                 #Assignment of the LED equal to 0
+            client.put_pixels(leds)                         #Use the Simulator_Fadecandy - LED's
+            time.sleep(2)                                   #Number in seconds - 1s
+        
+        break                                           #The break statement in Python terminates the current loop and resumes execution at the next statement
 
-            #First line (Empty)
+        time.sleep(2)                                   #Number in seconds - 1s
 
-            #Second Line
-        leds[110-led-2] = (204,102,0)
-        leds[110-led-1] = (204,102,0)
-        leds[110-led-0] = (204,102,0)
+    ##C. MOVEMENT OF THE BLOCK (DOWNWARDS) - YELLOW
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       
+    led = 0                                             #Assignment of the LED equal to 0
+    while True:                                         #While true loop = execute a block of code repeatedly until given boolean condition evaluated to False. If we write while True then the loop will run forever.
+        for rows in range(6):                           #For loop in range(X) movement
+            leds = [(255,255,255)]*360                  #Whole simulator Blue R,G,B (X,X,X) - WHITE
 
-        #Third line
-        leds[170-led-3] = (204,102,0)
-        leds[170-led-2] = (255,255,102)#Yellow
-        leds[170-led-1] = (204,102,0)
-        leds[170-led-0] = (255,255,102)#Yellow
-        leds[170-led+1] = (204,102,0)
+            leds[led+00 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 1 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 2 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 3 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 4 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 5 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 6 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 7 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 8 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+ 9 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+10 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+11 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+12 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+13 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+14 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+15 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+16 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+17 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+18 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+19 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+20 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+21 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+22 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+23 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+24 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+25 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+26 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+27 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+28 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+29 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
+            leds[led+30 + rows*60] = (255,255,51)       #Position of the LED - assignment the rows - Simulator color [R,G,B] - YELLOW
 
-        #Fourth Line
-        leds[230-led-4] = (204,102,0)
-        leds[230-led-3] = (204,102,0)
-        leds[230-led-2] = (255,0,0)
-        leds[230-led-1] = (255,0,0)
-        leds[230-led] = (255,0,0)
-        leds[230-led+1] = (204,102,0)
-        leds[230-led+2] = (204,102,0)
+            client.put_pixels(leds)                     #Use the Simulator_Fadecandy - LED's
+            time.sleep(.3)                              #Number in seconds - 0.1s
 
-        #Five Line
-        leds[290-led-2] = (255,128,0)
-        leds[290-led-1] = (255,128,0)
-        leds[290-led] = (255,128,0)
+        break                                           #The break statement in Python terminates the current loop and resumes execution at the next statement
 
-        #Sixth Line
-        leds[350-led-4] = (102,51,0)
-        leds[350-led-3] = (102,51,0)
 
-        leds[350-led+1] = (102,51,0)
-        leds[350-led+2] = (102,51,0)
 
+
+    ##D. Character 3 - The Grump (Mushroom) - Moving from right to left
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    led = 0
+    while led<60:                 
+        for led in range(54-7):
+                
+            leds = [(10,10,10)]*360 #Whole simulator White R,G,B (x,x,x)
+
+                #First line (Empty)
+
+                #Second Line
+            leds[110-led-2] = (204,102,0)
+            leds[110-led-1] = (204,102,0)
+            leds[110-led-0] = (204,102,0)
+
+            #Third line
+            leds[170-led-3] = (204,102,0)
+            leds[170-led-2] = (255,255,102)#Yellow
+            leds[170-led-1] = (204,102,0)
+            leds[170-led-0] = (255,255,102)#Yellow
+            leds[170-led+1] = (204,102,0)
+
+            #Fourth Line
+            leds[230-led-4] = (204,102,0)
+            leds[230-led-3] = (204,102,0)
+            leds[230-led-2] = (255,0,0)
+            leds[230-led-1] = (255,0,0)
+            leds[230-led] = (255,0,0)
+            leds[230-led+1] = (204,102,0)
+            leds[230-led+2] = (204,102,0)
+
+            #Five Line
+            leds[290-led-2] = (255,128,0)
+            leds[290-led-1] = (255,128,0)
+            leds[290-led] = (255,128,0)
+
+            #Sixth Line
+            leds[350-led-4] = (102,51,0)
+            leds[350-led-3] = (102,51,0)
+
+            leds[350-led+1] = (102,51,0)
+            leds[350-led+2] = (102,51,0)
+
+            client.put_pixels(leds)
+            time.sleep(.1)
+        break
+
+
+    ##E. Two colors from the outside will finish in the X number from the while loop (We can change the color from each side)
+    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    led = 0
+    while led<15: # si cambio a led<60 al cruzarse se cambian los colores
+        for rows in range(6):
+            leds[led + rows*60] = (0,255,0)
+            leds[59-led + rows*60] = (0,255,0)
         client.put_pixels(leds)
         time.sleep(.1)
-    break
+        led = led + 1
+    return
 
-#5. Two colors from the outside will finish in the middle with differents colors
-
-    #para dos colores, uno desde la hizquierda y otro desde la derrecha
-#que se paran en el medio
-led = 0
-while led<30: # si cambio a led<60 al cruzarse se cambian los colores
-    for rows in range(6):
-        leds[led + rows*60] = (0,255,0)
-        leds[59-led + rows*60] = (0,255,0)
-    client.put_pixels(leds)
-    time.sleep(.1)
-    led = led + 1
+A3()
