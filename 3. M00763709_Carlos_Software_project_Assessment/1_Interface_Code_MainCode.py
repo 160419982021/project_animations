7c99726c284e428949da9295bdd87fcc045ea60f
160419982021 <huallpafernandez@gmail.com>
2022-03-04 23:24:23 +0000

    Update

--- 3. M00763709_Carlos_Software_project_Assessment/1_Interface_Code_MainCode.py
+++ 3. M00763709_Carlos_Software_project_Assessment/1_Interface_Code_MainCode.py
@@ -1,8 +1,12 @@
-#Interface Code NO WORKING
-import opc
-import time
-import random
-import colorsys
+#Interface Code Main Code - Created by Carlos Huallpa - M00763709 - 2nd Year Robotics and Mechatronics
+
+#Website to change and modify colors (https://www.rapidtables.com/web/color/RGB_Color.html)
+
+
+import opc                                                                                      #Import Open Pixel Control
+import time                                                                                     #Import time access and conversions 
+import random                                                                                   #Import and generate pseudo-random numbers 
+import colorsys                                                                                 #Conversions between color systems
 
 s = 1.0 ##maximum colour
 v = 1.0 ##maximum brightness
@@ -1020,38 +1024,133 @@ Welcome to my code!!!
         for i in range(len(z)):                             #For loop in range(X)
             leds[z[i]] = (204,255,255)                      #Position of the LED - assignment - Simulator color [R,G,B] - Light White
             client.put_pixels(leds)                         #Use the Simulator_Fadecandy - LED's
-            time.sleep(0.1)                               #Number in seconds - 0.055s
+            time.sleep(0.1)                                 #Number in seconds - 0.1s
             
-
 
     def A8():
 
+        leds =[(0,0,0)]*360                                                                             #Simulator color [R,G,B] * numbers of leds working - YELLOW
 
-        for i in range(360): #this decides the final value(kind of) THIS AFFECTS THE LENGHT OF HOW MANY BALLS THERE WILL BE IN THE LINES OR THINGY
-                rgb_fractional = colorsys.hsv_to_rgb(start_hue+i/360.0, s, v) #colorsys returns floats between 0 and 1, THIS AFFECTS THE DIFFERENCE BETWEEN COLOURS BALLS AND DOESN'T CHANGE TOO MUCH BETWEEN THEM 
-                r,g,b = rgb_fractional #extract said floating point numbers and convert to rgb range
+        client = opc.Client('localhost:7890')                                                           #Use the Simulator_Fadecandy
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
+        client.put_pixels(leds)
+                 # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        led = 0
+        while led<60:
+
+            for rows in range(0,6): 
+                leds[59-led + rows*60] = (rows*50,70,240)
+            client.put_pixels(leds)
+            time.sleep(.001)
+            led = led + 1
+
+        time.sleep(2)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        led = 0
+        while led<60:
+                leds[0+led] = (0,255,0)
+                leds[60+led] = (0,255,0)
+                leds[120+led] = (0,255,0)
+                leds[239-led] = (0,255,0)
+                leds[299-led] = (0,255,0)
+                leds[359-led] = (0,255,0)
+                
+                time.sleep(.1)
+                client.put_pixels(leds)
+                led = led + 1
+
+        led = 0
+        while led<60:
+                leds[0+led] = (255,0,0)
+                leds[60+led] = (255,0,0)
+                leds[120+led] = (255,0,0)
+                leds[239-led] = (255,0,0)
+                leds[299-led] = (255,0,0)
+                leds[359-led] = (255,0,0)
+                
+                time.sleep(.1)
+                client.put_pixels(leds)
+                led = led + 2
+
+        time.sleep(1)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        leds = [(0,0,0)]*360 
+
+        client = opc.Client('localhost:7890')
+        client.put_pixels(leds)
+        client.put_pixels(leds)
+
+        s = 1.0 
+        v = 1.0 
+
+        for hue in range(0,360,2): 
+            rgb_fractional = colorsys.hsv_to_rgb(random.randint(hue-20, hue+20)/300.0, s, v)            #colorsys returns floats between 0 and 1
+            r_float = rgb_fractional[0]
+            g_float = rgb_fractional[1]
+            b_float = rgb_fractional[2]
+
+            rgb = (r_float*255, g_float*255, b_float*255)
+            leds[hue]=rgb
+            client.put_pixels(leds) 
+
+            time.sleep(0.00001)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        leds =[(255,255,255)]*360                                                                       #Simulator color [R,G,B] * numbers of leds working - WHITE
+
+        client = opc.Client('localhost:7890')                                                           #Use the Simulator_Fadecandy
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
+
+
+        s = 1.0 
+        v = 1.0 
+        start_hue = 50 
+
+        for i in range(360):                                                                            #this decides the final value(kind of) THIS AFFECTS THE LENGHT OF HOW MANY BALLS THERE WILL BE IN THE LINES OR THINGY
+                rgb_fractional = colorsys.hsv_to_rgb(start_hue+i/360.0, s, v)                           #colorsys returns floats between 0 and 1, THIS AFFECTS THE DIFFERENCE BETWEEN COLOURS BALLS AND DOESN'T CHANGE TOO MUCH BETWEEN THEM 
+                r,g,b = rgb_fractional                                                                  #extract said floating point numbers and convert to rgb range
                 leds[i] = (r*255,g*255,b*255)
-                client.put_pixels(leds) #assign
+                client.put_pixels(leds) 
         time.sleep(0.1)
 
+        # Left AND right (Black Swipe ) TO MIDDLE UNTIL LED NUMBER 30
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        time.sleep(3)
+        led = 0                                                                                         #Assignment of the LED equal to 0
+        while led<30:                                                                                   #While function (Close until 30)
+                for rows in range(6):                                                                   #For loop in range(X) ROWS - (1 to 6) outside to inside
+                    leds[led + rows*60] = (0,0,0)                                                       #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
+                    leds[59-led + rows*60] = (0,0,0)                                                    #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
+                client.put_pixels(leds)                                                                 #Use the Simulator_Fadecandy - LED's
+                time.sleep(.02)                                                                         #Number in seconds - 0.2s
+                led = led + 1                                                                           #Count +1 to the last number of led
 
-        ##B. Left to right (Cortain - Red)
-            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #Blinker Animation
+        led_list = [(0,0,0)]*360         
+        fade = 10                                                                                       # size of each fade step.
 
-        led=0                                               #Assignment of the LED equal to 0
-        for led in range(60):                               #For loop in range(X)
-                leds[led] = (255,153,51)                           #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                leds[led+60] = (255,153,51)                        #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                leds[led+120] = (255,153,51)                       #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                leds[led+180] = (255,153,51)                       #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                leds[led+180+60] = (255,153,51)                    #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                leds[led+120+180] = (255,153,51)                   #Position of the LED - assignment - Simulator color [R,G,B] - SLIGHTLY ORANGE
-                time.sleep (0.02)                               #Number in seconds - 1.5s
-                client.put_pixels(leds)
+        while True:
+            for led in enumerate(led_list): 
+                r,g,b = led[1]              
+                r = r+fade
+                #g = g+fade
+                b = b+fade
 
-            
+                new = (r,g,b)                                                                           # create new tuple containing the updated values
+                led_list[led[0]] = new                                                                  # place it in the original list at index led_num.
+
+                if r >=100 or r <= 1:                                                                   # check if the fade has hit its' limit;
+                    fade = -fade 
+
+            client.put_pixels(led_list)      
+            time.sleep(.0002)  
+
 
 #All the animations code finish HERE
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- 3. M00763709_Carlos_Software_project_Assessment/Animation8.py
+++ 3. M00763709_Carlos_Software_project_Assessment/Animation8.py
@@ -1,126 +1,136 @@
 #Animation8
 
 
-import opc
-import time
-import random
-import colorsys
+import opc                                                                                      #Import Open Pixel Control
+import time                                                                                     #Import time access and conversions 
+import random                                                                                   #Import and generate pseudo-random numbers 
+import colorsys                                                                                 #Conversions between color systems
+    
 
-leds = [(0,0,0)]*360
-client = opc.Client('localhost:7890')
-client.put_pixels(leds)
-client.put_pixels(leds)
+                                                                         #Use the Simulator_Fadecandy - LED's
 
 
+def A8():
 
+        leds =[(0,0,0)]*360                                                                             #Simulator color [R,G,B] * numbers of leds working - YELLOW
 
-led = 0
-while led<60:
-
-    for rows in range(0,6): 
-        leds[59-led + rows*60] = (rows*50,70,240)
-    client.put_pixels(leds)
-    time.sleep(.001)
-    led = led + 1
-
-time.sleep(2)
-
-led = 0
-while led<60:
-        leds[0+led] = (0,255,0)
-        leds[60+led] = (0,255,0)
-        leds[120+led] = (0,255,0)
-        leds[239-led] = (0,255,0)
-        leds[299-led] = (0,255,0)
-        leds[359-led] = (0,255,0)
-        
-        time.sleep(.1)
+        client = opc.Client('localhost:7890')                                                           #Use the Simulator_Fadecandy
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
         client.put_pixels(leds)
-        led = led + 1
+                 # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        led = 0
+        while led<60:
 
-led = 0
-while led<60:
-        leds[0+led] = (255,0,0)
-        leds[60+led] = (255,0,0)
-        leds[120+led] = (255,0,0)
-        leds[239-led] = (255,0,0)
-        leds[299-led] = (255,0,0)
-        leds[359-led] = (255,0,0)
-        
-        time.sleep(.1)
+            for rows in range(0,6): 
+                leds[59-led + rows*60] = (rows*50,70,240)
+            client.put_pixels(leds)
+            time.sleep(.001)
+            led = led + 1
+
+        time.sleep(2)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        led = 0
+        while led<60:
+                leds[0+led] = (0,255,0)
+                leds[60+led] = (0,255,0)
+                leds[120+led] = (0,255,0)
+                leds[239-led] = (0,255,0)
+                leds[299-led] = (0,255,0)
+                leds[359-led] = (0,255,0)
+                
+                time.sleep(.1)
+                client.put_pixels(leds)
+                led = led + 1
+
+        led = 0
+        while led<60:
+                leds[0+led] = (255,0,0)
+                leds[60+led] = (255,0,0)
+                leds[120+led] = (255,0,0)
+                leds[239-led] = (255,0,0)
+                leds[299-led] = (255,0,0)
+                leds[359-led] = (255,0,0)
+                
+                time.sleep(.1)
+                client.put_pixels(leds)
+                led = led + 2
+
+        time.sleep(1)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        leds = [(0,0,0)]*360 
+
+        client = opc.Client('localhost:7890')
         client.put_pixels(leds)
-        led = led + 2
+        client.put_pixels(leds)
 
-time.sleep(1)
+        s = 1.0 
+        v = 1.0 
 
-leds = [(0,0,0)]*360 #black
+        for hue in range(0,360,2): 
+            rgb_fractional = colorsys.hsv_to_rgb(random.randint(hue-20, hue+20)/300.0, s, v)            #colorsys returns floats between 0 and 1
+            r_float = rgb_fractional[0]
+            g_float = rgb_fractional[1]
+            b_float = rgb_fractional[2]
 
-client = opc.Client('localhost:7890')
-client.put_pixels(leds)
-client.put_pixels(leds)
+            rgb = (r_float*255, g_float*255, b_float*255)
+            leds[hue]=rgb
+            client.put_pixels(leds) 
 
-s = 1.0 
-v = 1.0 
+            time.sleep(0.00001)
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        leds =[(255,255,255)]*360                                                                       #Simulator color [R,G,B] * numbers of leds working - WHITE
 
-for hue in range(0,360,2): #(0,360,2)
-    rgb_fractional = colorsys.hsv_to_rgb(random.randint(hue-20, hue+20)/300.0, s, v) #colorsys returns floats between 0 and 1
-    r_float = rgb_fractional[0]
-    g_float = rgb_fractional[1]
-    b_float = rgb_fractional[2]
-
-    rgb = (r_float*255, g_float*255, b_float*255)
-    leds[hue]=rgb
-    client.put_pixels(leds) 
-
-    time.sleep(0.00001)
-
-leds =[(255,255,255)]*360                           #Simulator color [R,G,B] * numbers of leds working - WHITE
-
-client = opc.Client('localhost:7890')               #Use the Simulator_Fadecandy
-client.put_pixels(leds)                             #Use the Simulator_Fadecandy - LED's
-client.put_pixels(leds)                             #Use the Simulator_Fadecandy - LED's
+        client = opc.Client('localhost:7890')                                                           #Use the Simulator_Fadecandy
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
+        client.put_pixels(leds)                                                                         #Use the Simulator_Fadecandy - LED's
 
 
-s = 1.0 
-v = 1.0 
-start_hue = 50 
+        s = 1.0 
+        v = 1.0 
+        start_hue = 50 
 
-for i in range(360):                                                                  #this decides the final value(kind of) THIS AFFECTS THE LENGHT OF HOW MANY BALLS THERE WILL BE IN THE LINES OR THINGY
-	rgb_fractional = colorsys.hsv_to_rgb(start_hue+i/360.0, s, v)                   #colorsys returns floats between 0 and 1, THIS AFFECTS THE DIFFERENCE BETWEEN COLOURS BALLS AND DOESN'T CHANGE TOO MUCH BETWEEN THEM 
-	r,g,b = rgb_fractional                                                          #extract said floating point numbers and convert to rgb range
-	leds[i] = (r*255,g*255,b*255)
-	client.put_pixels(leds) 
-time.sleep(0.1)
+        for i in range(360):                                                                            #this decides the final value(kind of) THIS AFFECTS THE LENGHT OF HOW MANY BALLS THERE WILL BE IN THE LINES OR THINGY
+                rgb_fractional = colorsys.hsv_to_rgb(start_hue+i/360.0, s, v)                           #colorsys returns floats between 0 and 1, THIS AFFECTS THE DIFFERENCE BETWEEN COLOURS BALLS AND DOESN'T CHANGE TOO MUCH BETWEEN THEM 
+                r,g,b = rgb_fractional                                                                  #extract said floating point numbers and convert to rgb range
+                leds[i] = (r*255,g*255,b*255)
+                client.put_pixels(leds) 
+        time.sleep(0.1)
 
-##D. Left AND right (Black Swipe ) TO MIDDLE UNTIL LED NUMBER 30
-    ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Left AND right (Black Swipe ) TO MIDDLE UNTIL LED NUMBER 30
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-led = 0                                             #Assignment of the LED equal to 0
-while led<30:                                       #While function (Close until 30)
-        for rows in range(6):                           #For loop in range(X) ROWS - (1 to 6) outside to inside
-            leds[led + rows*60] = (0,0,0)               #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
-            leds[59-led + rows*60] = (0,0,0)            #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
-        client.put_pixels(leds)                         #Use the Simulator_Fadecandy - LED's
-        time.sleep(.02)                                 #Number in seconds - 0.2s
-        led = led + 1                                   #Count +1 to the last number of led
+        led = 0                                                                                         #Assignment of the LED equal to 0
+        while led<30:                                                                                   #While function (Close until 30)
+                for rows in range(6):                                                                   #For loop in range(X) ROWS - (1 to 6) outside to inside
+                    leds[led + rows*60] = (0,0,0)                                                       #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
+                    leds[59-led + rows*60] = (0,0,0)                                                    #Position of the LED and rows - assignment - Simulator color [R,G,B] - BLACK
+                client.put_pixels(leds)                                                                 #Use the Simulator_Fadecandy - LED's
+                time.sleep(.02)                                                                         #Number in seconds - 0.2s
+                led = led + 1                                                                           #Count +1 to the last number of led
 
+        # 
+            ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        #Blinker Animation
+        led_list = [(0,0,0)]*360         
+        fade = 10                                                                                       # size of each fade step.
 
-#Blinker Animation
-led_list = [(0,0,0)]*360         
-fade = 10                     # size of each fade step.
+        while True:
+            for led in enumerate(led_list): 
+                r,g,b = led[1]              
+                r = r+fade
+                #g = g+fade
+                b = b+fade
 
-while True:
-    for led in enumerate(led_list): 
-        r,g,b = led[1]              
-        r = r+fade
-        #g = g+fade
-        b = b+fade
+                new = (r,g,b)                                                                           # create new tuple containing the updated values
+                led_list[led[0]] = new                                                                  # place it in the original list at index led_num.
 
-        new = (r,g,b)            # create new tuple containing the updated values
-        led_list[led[0]] = new   # place it in the original list at index led_num.
+                if r >=100 or r <= 1:                                                                   # check if the fade has hit its' limit;
+                    fade = -fade 
 
-        if r >=100 or r <= 1:           # check if the fade has hit its' limit;
-            fade = -fade 
-
-    client.put_pixels(led_list)      
-    time.sleep(.0002)  
+            client.put_pixels(led_list)      
+            time.sleep(.0002)  
+A8()
